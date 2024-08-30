d7eddec09674b6ee27e7d1ee94a1c74cd558d159
Vitor Musolino <vtteixeira02@gmail.com>
2024-08-30 17:53:36 -0300

    Detalhes

--- src/Componentes/IntroducaoSobre/index.jsx
+++ src/Componentes/IntroducaoSobre/index.jsx
@@ -4,7 +4,7 @@ export default function IntroducaoSobre() {
             <div className="introducao">
                 <h1>Bem-vindo à revolução dos vídeos!</h1>
                 <p>Somos uma startup com o objetivo de facilitar a produção de conteúdo em larga escala com qualidade profissional, capaz de atingir uma audiência de milhões de seguidores.</p>
-             <a href="/sobre">Baixe o App</a>
+             <a href="/contato">Baixe o App</a>
             </div>
             <div className="flex-image">
                 <img src="../public/tela1.png" alt="" />
